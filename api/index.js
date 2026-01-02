0a0e2d6b5d64ace2b52994c2f97713bfc7127ea3
Zennon <zeitaa34@gmail.com>
2026-01-02 18:08:01 -0300

    Configuration to deploy

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ const prismaConnect = async () => {
     await prisma.$connect();
     console.log(" Conectado a la base de datos con Prisma");
 
-    app.listen(3000, () => {
+    /*app.listen(3000, () => {
       console.log("Server listening on port 3000");
-    });
+    });*/
   } catch (error) {
     console.error(" Error al iniciar:", error);
   }
